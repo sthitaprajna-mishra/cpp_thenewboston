8eb619a2938fc42b04ef00ce88a34ce7088bec9f
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2020-12-04 18:44:03 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	
+	cout << "Enter a number boss! \n";
+	cin >> a;
+	
+	cout << "Enter another number \n";
+	cin >> b;
+	
+	sum = a + b;
+	
+	cout << "the sum is: " << sum << endl;
+	
+	return 0;
+}
+
+
+
+
+
