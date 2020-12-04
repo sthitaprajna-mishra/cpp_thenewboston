136446f9ee4cd1e4e0aab84442f0bdfe25d35d73
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2020-12-04 18:58:09 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	if(8 > 2)
+	{
+		cout << "Bucky is awesome" << endl;
+	}
+	
+	int a = 98;
+	int b = 78;
+	
+	if(a > b)
+	{
+		cout << "a > b : " << a << " > " << b << endl;
+	}
+	
+	return 0;
+}
